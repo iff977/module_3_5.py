38ebc7d3353d9e54aed1c669a90223a6ac0d01f9
iff977 <ilgizar.shigapoff1980@gmail.com>
2024-11-26 20:27:49 +0400

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,7 +3,8 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    if first != 0:
         return first
+
 result = get_multiplied_digits(40203)
 print(result)
